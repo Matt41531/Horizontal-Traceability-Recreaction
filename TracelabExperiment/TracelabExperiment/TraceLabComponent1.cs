509315af4d33de905f9c87b69c1fceffc9bd1000
Matt41531 <dxfan218@yahoo.com>
2019-01-27 10:18:43 -0500

    Created initial helloworld component and solution

---
+++ TracelabExperiment/TracelabExperiment/TraceLabComponent1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// Located in c:\Program Files (x86)\COEST\TraceLab\lib\TraceLabSDK.dll
+using TraceLabSDK;
+
+namespace TracelabExperiment
+{
+    [Component(Name = "FeatureRequestsTest",
+                Description = "",
+                Author = "Matthew Rife & Jen Lee",
+                Version = "1.0")]
+    //[IOSpec(IOType = IOSpecType.Output, Name = "outputName", DataType = typeof(int))]
+    public class TraceLabComponent1 : BaseComponent
+    {
+        public TraceLabComponent1(ComponentLogger log) : base(log) { }
+
+        public override void Compute()
+        {
+            // your component implementation
+            Logger.Trace("Hello World");
+
+            //Workspace.Store("outputName", 5);
+        }
+    }
+}

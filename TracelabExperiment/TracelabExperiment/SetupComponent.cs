a182419fda9a52e56ef00cdfd7ad88803cd7f9fc
Jen Lee <jmle265@g.uky.edu>
2019-02-05 11:17:31 -0500

    renaming files

---
+++ TracelabExperiment/TracelabExperiment/SetupComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+// Located in c:\Program Files (x86)\COEST\TraceLab\lib\TraceLabSDK.dll
+using TraceLabSDK;
+using TraceLabSDK.Types;
+
+namespace RecreationExperiment
+{
+    [Component(Name = "FeatureRequestsTest",
+                Description = "",
+                Author = "Matthew Rife & Jen Lee",
+                Version = "2.0",
+                ConfigurationType = typeof(SetupComponentConfiguration)
+        )]
+    [IOSpec(IOType = IOSpecType.Output, Name = "outputName", DataType = typeof(TLArtifactsCollection))]
+    public class SetupComponent : BaseComponent
+    {
+        public SetupComponent(ComponentLogger log) : base(log)
+        {
+            this.Configuration = new SetupComponentConfiguration();
+        }
+
+        public new SetupComponentConfiguration Configuration
+        {
+            get => base.Configuration as SetupComponentConfiguration;
+            set => base.Configuration = value;
+        }
+        public override void Compute()
+        {
+            // your component implementation
+    
+            var inputFile = this.Configuration.Artifacts.Absolute;
+            
+            string strCmdText;
+            strCmdText = "/C ipconfig/all";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            Logger.Trace(inputFile);
+            Logger.Trace("Worked");
+            
+            //Workspace.Store("outputName", 5);
+        }
+    }
+}
---
+++ TracelabExperiment/TracelabExperiment/SetupComponentConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using TraceLabSDK.Component.Config;
+
+namespace RecreationExperiment
+{
+    public class SetupComponentConfiguration
+    {
+        [DisplayName("Artifacts location")]
+        [Description("Location of artifacts file")]
+        public FilePath Artifacts { get; set; }
+
+        [DisplayName("Output Directory")]
+        [Description("Directory for output files")]
+        public DirectoryPath OutputDirectory { get; set; }
+
+    }
+}

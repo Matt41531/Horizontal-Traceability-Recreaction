91503cf4c2870b7e977859f377749632ac4f048a
Matt41531 <dxfan218@yahoo.com>
2019-02-11 14:35:04 -0500

    Added changes from Matthew branch to setupComponent

--- ComponentSolutions/SetupComponent/SetupComponent.cs
+++ ComponentSolutions/SetupComponent/SetupComponent.cs
@@ -12,7 +12,7 @@ namespace ComponentSolutions
     [Component(Name = "Setup",
                 Description = "",
                 Author = "Matthew Rife & Jen Lee",
-                Version = "2.0",
+                Version = "2.1",
                 ConfigurationType = typeof(SetupComponentConfiguration)
         )]
     [IOSpec(IOType = IOSpecType.Output, Name = "outputName", DataType = typeof(TLArtifactsCollection))]
@@ -31,12 +31,31 @@ namespace ComponentSolutions
         public override void Compute()
         {
             // your component implementation
-    
+            try
+            { 
+                Logger.Trace(this.Configuration.Artifacts.Absolute);
+            }
+            catch (Exception e)
+            {
+                Logger.Trace("Error: Missing input artifact", e);
+                return;
+            }
+            //Run command line tool with parameters
             var inputFile = this.Configuration.Artifacts.Absolute;
-            
+            try
+            { 
+                Logger.Trace(this.Configuration.OutputDirectory.Absolute);
+            }
+            catch (Exception e)
+            { 
+                Logger.Trace("Error: Invalid output directory", e);
+            }
+            var outputDirectory = this.Configuration.OutputDirectory.Absolute;
             string strCmdText;
+            string strStartingText = "/C ";
             strCmdText = "/C ipconfig/all";
-            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            System.Diagnostics.Process.Start("CMD.exe", (strStartingText + inputFile));
+            //DEBUGGING prints        
             Logger.Trace(inputFile);
             Logger.Trace("Worked");
             

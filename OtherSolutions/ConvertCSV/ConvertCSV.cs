86d01f49d9ceb132275252ce952eda05336e684d
Jen Lee <jmle265@g.uky.edu>
2019-02-17 13:51:33 -0500

    Formatting *.csv datasets

---
+++ OtherSolutions/ConvertCSV/ConvertCSV.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConvertCSV
+{
+    class ConvertCSV
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // ****** Variable that changes *****
+                // Options: mylyn, argouml, or netbeans
+                var dataset = "netbeans";
+                // **********************************
+                var filename = "";
+
+                // Assign filename
+                if (dataset == "mylyn")
+                    filename = "Mylyn_cossim_LO_SP_DW_SM_SC_AC";
+                else if (dataset == "argouml")
+                    filename = "ArgoUML_cossim_LO_SP_DW_SM_SC_AC";
+                else if (dataset == "netbeans")
+                    filename = "Netbeans_Top50_TFIDF_LO_SP_DW_SM_SC_AC";
+
+                // open csv, replace all ',' with '.'
+                using (StreamReader sr = new StreamReader(@"../../../../Databases/" + filename + ".csv"))
+                {
+                    String line = sr.ReadToEnd();
+
+                    char oldChar = ',';
+                    char newChar = '.';
+
+                    String newline = line.Replace(',', '.');
+
+                    var newfile = new StringBuilder();
+
+                    newfile.Append(newline);
+
+                    File.WriteAllText(@"../../../../Databases/" + filename + "_formatted.csv", newfile.ToString());
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The File could not be read:");
+                Console.WriteLine(e.Message);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
